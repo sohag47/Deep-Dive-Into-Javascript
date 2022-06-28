387531d5f1c5c6cf8232f35e110d2049c03b1d28
Minhazul Islam Sohag <minhazulislamsohag47@gmail.com>
2022-06-28 17:15:53 +0600

    show output in  javascript many ways

---
+++ Fundamental/Basic/print.js
@@ -0,0 +1,8 @@
+//! Welcome to javascript tutorial
+
+//? Print Data
+console.log("Hello JavaScript");
+document.getElementById("demo").innerHTML = 5 + 6;
+document.write(5 + 6);
+window.alert(5 + 6);
+window.print();
