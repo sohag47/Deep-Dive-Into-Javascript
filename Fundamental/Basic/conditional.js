99def7c7cc3778eeb91977dca1de8fa4e9cfb511
sohag47 <minhazul15-1630@diu.edu.bd>
2022-07-01 07:14:32 +0600

    conditional

---
+++ Fundamental/Basic/conditional.js
@@ -0,0 +1,32 @@
+let time = 10;
+
+if (time < 10) {
+    greeting = "Good morning";
+  } else if (time < 20) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }
+console.log(greeting);
+  
+// console.log(new Date().getTime());
+
+//? Switch case
+var grade='F';  
+var result;  
+
+switch(grade){  
+case 'A':  
+    result="A Grade";  
+    break;  
+case 'B':  
+    result="B Grade";  
+    break;  
+case 'C':  
+    result="C Grade";  
+    break;  
+default:  
+    result="No Grade";  
+} 
+
+console.log(result);
