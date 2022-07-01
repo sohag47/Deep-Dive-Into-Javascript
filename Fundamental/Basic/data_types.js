6a05673a010d342a9eb2d7c30657565daea96f40
sohag47 <minhazul15-1630@diu.edu.bd>
2022-07-01 07:14:07 +0600

    data type

---
+++ Fundamental/Basic/data_types.js
@@ -0,0 +1,16 @@
+//! JavaScript Data Types
+//? JavaScript primitive data types
+/*
+* 1. (String)	represents sequence of characters e.g. "hello"
+* 2. (Number)	represents numeric values e.g. 100
+* 3. (Boolean)	represents boolean value either false or true
+* 4. (Undefined)	represents undefined value
+* 5. (Null)	represents null i.e. no value at all
+*/
+
+//? JavaScript non-primitive data types
+/*
+* 1. (Object)	represents instance through which we can access members
+* 2. (Array)	represents group of similar values
+* 3. (RegExp)	represents regular expression
+*/
