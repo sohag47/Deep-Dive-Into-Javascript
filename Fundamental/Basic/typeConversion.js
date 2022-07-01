646f09e1f11f6b3c72ce869a338811ff0a4f511f
sohag47 <minhazul15-1630@diu.edu.bd>
2022-07-01 08:42:43 +0600

    type conversion implemented

---
+++ Fundamental/Basic/typeConversion.js
@@ -0,0 +1,21 @@
+//! Type Conversion
+/*
+*1 . String()
+*2. Number()
+*3. Boolean()
+*4. parseFloat()
+*5. parseInt()
+*/
+
+//! String Conversion
+let value = true;
+console.log("🚀 ~ file: typeConversion.js ~ line 3 ~ value", typeof value)
+
+value = String(value)
+console.log("🚀 ~ file: typeConversion.js ~ line 3 ~ value", typeof value)
+
+value = Number(value)
+console.log("🚀 ~ file: typeConversion.js ~ line 3 ~ value", typeof value)
+
+value = Boolean(value)
+console.log("🚀 ~ file: typeConversion.js ~ line 3 ~ value", typeof value)
